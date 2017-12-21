30f289ce1add1eb35b571418301df14677fe3083
Igor Matos <igor.matos@liferay.com>
2017-12-21 11:00:34 -0300

    modelo do ponto

---
+++ js/modelos/ponto.js
@@ -0,0 +1,61 @@
+function Ponto(x,y,z) {
+    this.x = x;
+    this.y = y;
+    this.normal = new Vetor(0, 0, 0);
+
+    if (z != undefined) {
+        this.z = z;
+    }
+    
+    this.sub = function(p) {
+        return new Ponto(this.x - p.x, this.y - p.y, this.z - p.z);
+    };
+
+    this.add = function(p) {
+        return new Ponto(this.x + p.x, this.y + p.y, this.z + p.z);
+    };
+    
+    this.multiplicarMatrix = function(matrix) {
+        var x = this.x*matrix[0][0] + this.y*matrix[0][1] + this.z*matrix[0][2];
+        var y = this.x*matrix[1][0] + this.y*matrix[1][1] + this.z*matrix[1][2];
+        var z = this.x*matrix[2][0] + this.y*matrix[2][1] + this.z*matrix[2][2];
+        return new Ponto(x, y, z);
+    };
+
+    this.getPontoVista = function(camera) {
+        var a = this.clone();
+        var b = a.sub(camera.c);
+        var r = b.multiplicarMatrix(camera.alfa);
+        return r;
+    };
+    
+    this.getPontoTela = function(camera) {
+        var x = (camera.d/camera.hx)*(this.x/this.z);
+        var y = (camera.d/camera.hy)*(this.y/this.z);
+        var a = new Ponto(x, y);
+        var r = new Ponto(((a.x + 1) * (largura / 2)), ((1 - a.y) * (altura / 2)));
+        r.x = Math.round(r.x);
+        r.y = Math.round(r.y);
+        r.normal = this.normal.clone();
+        return r;
+    };
+    
+    this.multiplicar = function(k) {
+        return new Ponto(this.x*k, this.y*k, this.z*k);
+    };
+    
+    this.clone = function() {
+        if (this.z == undefined){
+            return new Ponto(this.x, this.y);    
+        }
+        
+        return new Ponto(this.x, this.y, this.z);
+    };
+
+    
+
+
+
+
+
+}
--- js/modelos/vetor.js
+++ js/modelos/vetor.js
@@ -1,7 +1,63 @@
-class Vetor {
-  constructor(x, y,z) {
+function Vetor(x, y,z) {
     this.x = x;
     this.y = y;
     this.z = z;
-  }
+
+  this.norma = function() {
+    var n = this.produtoEscalar(this);
+    var ret = Math.sqrt(n);
+    return ret;
+  };
+
+  this.getCosseno = function(v) {
+    var cos = (v.produtoEscalar(this))/(this.norma()*v.norma());
+    return cos;
+  };
+
+  this.normalizar = function() {
+    var sum = (this.x*this.x + this.y*this.y + this.z*this.z);
+    if(!sum) return;
+    var normal = Math.sqrt(sum);
+    this.x/=normal;
+    this.y/=normal;
+    this.z/=normal;
+  };
+  
+  this.produtoEscalar = function(v) {
+    return (this.x*v.x + this.y*v.y + this.z*v.z);
+  };
+
+  this.produtoVetorial = function(v) {
+    var x = this.y*v.z - this.z*v.y;
+    var y = this.z*v.x - this.x*v.z;
+    var z = this.x*v.y - this.y*v.x;
+    return new Vetor(x, y, z);
+  };
+
+  this.multiplicar = function(k) {
+    return new Vetor(this.x*k, this.y*k, this.z*k);
+  };
+
+  this.projecaoOrtogonal = function(v) {
+    var k = (v.produtoEscalar(this))/(this.produtoEscalar(this));
+    var r = new Vetor(this.x, this.y, this.z);
+    r = r.multiplicar(k);
+    return r;
+  };
+  
+  this.add = function(v) {
+    return new Vetor(this.x+v.x, this.y+v.y, this.z+v.z);
+  };
+
+  this.sub = function(v) {
+    return new Vetor(this.x-v.x, this.y-v.y, this.z-v.z);
+  };
+  
+  this.gramSchmidt = function(v) {
+    return this.sub(v.projecaoOrtogonal(this));
+  };
+  
+  this.clone = function () {
+    return new Vetor(this.x, this.y, this.z);
+  };
 }
